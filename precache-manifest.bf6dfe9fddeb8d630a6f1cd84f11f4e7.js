2beb7b04a1d78ef2440c0cf6d3ae6d6dee668df1
JesusRubio <jesusarizarubio@gmail.com>
2020-11-22 19:07:58 -0600

    Updates

---
+++ precache-manifest.bf6dfe9fddeb8d630a6f1cd84f11f4e7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d6077df64acd14173e6adb0571ec2aa",
+    "url": "/Employee-Directory/index.html"
+  },
+  {
+    "revision": "e9907e795a4d811cab1e",
+    "url": "/Employee-Directory/static/css/2.437d98bc.chunk.css"
+  },
+  {
+    "revision": "4fe0a9251493478b148e",
+    "url": "/Employee-Directory/static/css/main.8c316816.chunk.css"
+  },
+  {
+    "revision": "e9907e795a4d811cab1e",
+    "url": "/Employee-Directory/static/js/2.0a6ff45d.chunk.js"
+  },
+  {
+    "revision": "4fe0a9251493478b148e",
+    "url": "/Employee-Directory/static/js/main.d3b25cb5.chunk.js"
+  },
+  {
+    "revision": "1794aedb7da2e07d4918",
+    "url": "/Employee-Directory/static/js/runtime~main.51f3d348.js"
+  }
+]);
